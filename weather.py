3d6e1a2a961b1a2a2a5afd136d1015882ac30e01
JNuevo-LS <145623416+JNuevo-LS@users.noreply.github.com>
2023-10-11 19:06:06 -0400

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,9 @@
 import geocoder
 import requests
-from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QToolTip, QHBoxLayout, QVBoxLayout
-from PyQt6.QtGui import QFont
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QToolTip
+from PyQt6.QtGui import QFont, QPalette
 import sys
 import datetime as dt
-
 #Class set up to make extracting data from the OpenWeatherMap API's data easier.
 class weather_date:
     def __init__(self, weather_data):
@@ -41,11 +40,26 @@ app = QApplication(sys.argv)
 window = QWidget()
 #Overall window configuration
 window.setWindowTitle("PyQt App")
-window.setGeometry(100, 100, 400, 600)
+window.setFixedSize(1140, 600)
+window.setWindowTitle('Weather App')
 
 first_day = ''
 
 #Function to update the weather data
+
+def pseudo_calendar(value):
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+    value = int(value)
+    if value <= 12:
+        value_as_month = months[value-1]
+    else: print('Month value invalid')
+    return value_as_month
+
+def readable_date(date_given):
+    date = dt.datetime.fromtimestamp(date_given)
+    date = (str(date).split())[0].split('-')
+    month = pseudo_calendar(date[1])
+    return [date, month]
 
 def Update_Weather():
     g = geocoder.ip('me')
@@ -62,20 +76,46 @@ QToolTip.setFont(QFont('SansSerif', 10))
 update_button = QPushButton(text='Update Meteorological Data', parent= window)
 update_button.clicked.connect(lambda:Update_Weather())
 update_button.setToolTip('Please keep in mind that the data is refreshed every 30 minutes.')
-update_button.move(230, 550)
+update_button.move(390, 550)
+
+week_date_list = []
+for n in range(7): #Iterates through every day of the week, and gathers their timestamp.
+    week_date_list.append(week_data.timestamps[n]) 
+
+readable_date_list = []
+for n in range(7): #Iterates through list of timestamps and turns them into a readable date.
+    readable_date_list.append(readable_date(week_date_list[n]))
 
 
+def create_label(day_of_week):
+    date_label = QLabel(text = f'Forecast for {day_of_week[1]} {day_of_week[0][2]}, {day_of_week[0][0]}', parent = window)
+    date_label.setFont(QFont('SansSerif', 8))
+    return date_label
 
-today = dt.datetime.fromtimestamp(week_data.timestamps[0])
-today_date = QLabel(text = '', parent = window )
-# today_date.setFont(QFont('SansSerif', 7))
-today_date.move(50, 50)
 
-Day1_Data = QLabel(text='The temperature today will be ' + str(week_data.day_temps[0]) + ' degrees Fahrenheit', parent = window, wordWrap = True)
-Day1_Data.setFont(QFont('SansSerif', 7))
-Day1_Data.resize(100,200)
-Day1_Data.move(10, 40)
+today_date = create_label(readable_date_list[0])
+today_date.move(15, 50)
 
+day2_date = create_label(readable_date_list[1])
+day2_date.move(175,50)
+day3_date = create_label(readable_date_list[2])
+day3_date.move(335, 50)
+day4_date = create_label(readable_date_list[3])
+day4_date.move(495, 50)
+day5_date = create_label(readable_date_list[4])
+day5_date.move(655, 50)
+day6_date = create_label(readable_date_list[5])
+day6_date.move(815, 50)
+day7_date = create_label(readable_date_list[6])
+day7_date.move(975, 50)
+
+
+today_Data = QLabel(text='The temperature today will be ' + str(week_data.day_temps[0]) + ' degrees Fahrenheit', parent = window, wordWrap = True)
+today_Data.setFont(QFont('SansSerif', 7))
+today_Data.resize(120,30)
+today_Data.move(20, 70)
+
+app.setStyle('Fusion')
 
 window.show()
 sys.exit(app.exec())
